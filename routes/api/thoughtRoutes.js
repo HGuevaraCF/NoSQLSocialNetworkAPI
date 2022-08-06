91a58c4df492a92a746732354623483e0b16f8c8
HGuevaraCF <87338085+HGuevaraCF@users.noreply.github.com>
2022-08-05 23:38:09 -0500

    thoughts routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,10 +1,16 @@
 const router = require('express').Router();
-const { getThoughts, getSingleThought, createThought, updateThought, deleteThought } = require('./../../controllers/thoughtsController');
+const { getThoughts, getSingleThought, createThought, updateThought, deleteThought, createReaction, deleteReaction } = require('./../../controllers/thoughtsController');
 
 //api/thoughts
 router.route('/').get(getThoughts).post(createThought);
 
-//api/thoughts/:id
+//api/thoughts/:toughtId
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+//api/thoughts/:thoughtId/reactions
+router.route('/:thoughthId/reactions').put(createReaction);
+
+//api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughthId/reactions/:reactionId').put(deleteReaction);
 
 module.exports = router;
